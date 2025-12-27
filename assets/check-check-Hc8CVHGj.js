5e4d00d4deaec7dc247726ef1fa59486ae659754
Viega03-KH <ahmadov2000000@gmail.com>
2025-12-27 15:57:57 +0500

    ----

---
+++ assets/check-check-Hc8CVHGj.js
@@ -0,0 +1,6 @@
+import{k as e}from"./index-BtXiaCr0.js";/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=e("check-check",[["path",{d:"M18 6 7 17l-5-5",key:"116fxf"}],["path",{d:"m22 10-7.5 7.5L13 16",key:"ke71qq"}]]);export{k as C};
