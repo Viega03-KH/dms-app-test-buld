82c9347dc29f223fbdeb296c3beba132a6bfb8f9
Viega03-KH <ahmadov2000000@gmail.com>
2025-12-30 15:51:44 +0500

    ------

---
+++ assets/UInfoItem-CrU6A23y.js
@@ -0,0 +1,6 @@
+import{k as l,d as c,a as i,o as u,b as a,v as o,n as m}from"./index-DirDpiKz.js";/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=l("search",[["path",{d:"m21 21-4.34-4.34",key:"14j7rj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}]]),d={class:"text-[#697696] text-sm font-medium"},p={class:"text-dark text-md font-bold"},k={__name:"UInfoItem",props:{label:String,value:[String,Number]},setup(t){const s=c(!1);return(r,e)=>(u(),i("div",{class:m([r.$attrs.class,"relative cursor-pointer"]),onMouseenter:e[0]||(e[0]=n=>s.value=!0),onMouseleave:e[1]||(e[1]=n=>s.value=!1)},[a("p",d,o(t.label),1),a("p",p,o(t.value),1)],34))}};export{f as S,k as _};
